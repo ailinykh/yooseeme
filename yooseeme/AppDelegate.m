036fc47177b3e7ee7b9152e298660d92af165abd
Anthony <anthonyilinykh@gmail.com>
2016-09-28 18:36:59 +0300

    alert notifications

--- yooseeme/AppDelegate.m
+++ yooseeme/AppDelegate.m
@@ -14,8 +14,12 @@
 #import "P2PClient.h"
 #import "Contact.h"
 #import "ContactDAO.h"
+#import "PAIOUnit.h"
 
-@interface AppDelegate ()
+@interface AppDelegate () {
+    UIBackgroundTaskIdentifier _backgroundTaskID;
+    AVAudioPlayer *_audioPlayer;
+}
 @property (strong, nonatomic) NSString *token;
 @end
 
@@ -34,17 +38,36 @@
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    if ([[P2PClient sharedClient] p2pCallState] == P2PCALL_STATUS_READY_P2P) {
+        [[P2PClient sharedClient] setP2pCallState:P2PCALL_STATUS_NONE];
+        [[PAIOUnit sharedUnit] stopAudio];
+    }
+    [[P2PClient sharedClient] p2pHungUp];
+    [[P2PClient sharedClient] rtspHungUp];
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    _backgroundTaskID = [application beginBackgroundTaskWithExpirationHandler:^{
+        [[P2PClient sharedClient] p2pDisconnect];
+        [application endBackgroundTask:_backgroundTaskID];
+    }];
+    
+    if (_backgroundTaskID == UIBackgroundTaskInvalid) {
+        [[P2PClient sharedClient] p2pDisconnect];
+        NSLog(@"Failed to start background task!");
+        return;
+    }
+    
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        while (_backgroundTaskID != UIBackgroundTaskInvalid) {
+            sleep(1.0);
+        }
+    });
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    application.applicationIconBadgeNumber = 0;
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
@@ -113,18 +136,47 @@
     NSString *typeStr = NSLocalizedString(@"motion_dect_alarm", nil);
     
     NSLog(@"ALARM MESSAGE RECEIVED!");
-    UILocalNotification *alarmNotify = [[UILocalNotification alloc] init];
-    alarmNotify.fireDate = [NSDate dateWithTimeIntervalSinceNow:0];
-    alarmNotify.timeZone = [NSTimeZone defaultTimeZone];
-//    alarmNotify.soundName = [self playAlarmMessageRingWithAlarmType:type isBeBackground:YES];
-    if ([contactId isEqualToString:contactName] || contactName == nil) {
-        alarmNotify.alertBody = [NSString stringWithFormat:@"%@:%@",contactId,typeStr];
-    }else{
-        alarmNotify.alertBody = [NSString stringWithFormat:@"%@:%@",contactName,typeStr];
+    
+    if (_backgroundTaskID != UIBackgroundTaskInvalid) {
+        UILocalNotification *alarmNotify = [[UILocalNotification alloc] init];
+        alarmNotify.fireDate = [NSDate dateWithTimeIntervalSinceNow:0];
+        alarmNotify.timeZone = [NSTimeZone defaultTimeZone];
+        alarmNotify.soundName = [self playAlarmMessageRingWithAlarmType:13 isBeBackground:YES];
+        if ([contactId isEqualToString:contactName] || contactName == nil) {
+            alarmNotify.alertBody = [NSString stringWithFormat:@"%@:%@",contactId,typeStr];
+        }else{
+            alarmNotify.alertBody = [NSString stringWithFormat:@"%@:%@",contactName,typeStr];
+        }
+        alarmNotify.applicationIconBadgeNumber = 1;
+        alarmNotify.alertAction = NSLocalizedString(@"open", nil);
+        [[UIApplication sharedApplication] scheduleLocalNotification:alarmNotify];
     }
-    alarmNotify.applicationIconBadgeNumber = 1;
-    alarmNotify.alertAction = NSLocalizedString(@"open", nil);
-    [[UIApplication sharedApplication] scheduleLocalNotification:alarmNotify];
+    else {
+        [self playAlarmMessageRingWithAlarmType:13 isBeBackground:NO];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            UIAlertController *ac = [UIAlertController alertControllerWithTitle:@"Alert"
+                                                                        message:@"Motion detected"
+                                                                 preferredStyle:UIAlertControllerStyleAlert];
+            [ac addAction:[UIAlertAction actionWithTitle:@"Ok" style:UIAlertActionStyleDefault handler:nil]];
+            [_window.rootViewController presentViewController:ac animated:YES completion:nil];
+        });
+    }
+}
+
+-(NSString *)playAlarmMessageRingWithAlarmType:(int)alarmType isBeBackground:(BOOL)isBackground {
+    if (isBackground) {
+        return @"alarm_push_ring.caf";
+    }
+    
+    NSURL *ringUrl = [NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"alarm_push_ring" ofType:@"caf"]];
+    
+    [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:nil];
+    _audioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:ringUrl error:nil];
+    [_audioPlayer prepareToPlay];
+    [_audioPlayer setVolume:1.0f];
+    [_audioPlayer setNumberOfLoops:0];
+    [_audioPlayer play];
+    return nil;
 }
 
 @end
